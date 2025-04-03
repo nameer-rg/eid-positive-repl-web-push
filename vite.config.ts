1406d50202c994f0f480ec98ba07e2819fb30ddb
nameer-rg <58900809+nameer-rg@users.noreply.github.com>
2025-04-03 19:26:01 +0000

    cleaned vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  // Use GitHub Pages subpath only in production builds
 
   server: {
     host: "::",
